859f40a489b33c7ec235bf7c266a0f0266e727c7
weebaayush <ayushgautam00004@gmail.com>
2026-07-18 18:07:38 +0530

    feat(ui): export reusable UI components

---
+++ src/components/ui/index.ts
@@ -0,0 +1,21 @@
+export { Badge } from './Badge'
+export type { BadgeProps } from './Badge'
+
+export { Button } from './Button'
+export type { ButtonProps } from './Button'
+
+export {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './Card'
+export type { CardProps, CardSectionProps } from './Card'
+
+export { Input } from './Input'
+export type { InputProps } from './Input'
+
+export { TextArea } from './TextArea'
+export type { TextAreaProps } from './TextArea'
